9b8e11e1e06db7db15518e2d6ad0bbf8aebe0958
zenger7 <nawaaf.almansour@gmail.com>
2019-05-15 03:57:42 +0300

    created game logic

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ console.log("Up and running!");
 
 var cardOne = "queen";
 console.log("User flipped " + cardOne);
+alert('Hello, friends.');
+var cards =["queen", "queen", "king", "king"];
+var cardsInPlay =[];
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+
+  }else {
+
+  }
+}
